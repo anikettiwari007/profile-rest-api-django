f21671571aee3eb5eb2611747ff4b6e8cd1da97a
Aniket <ignou.mt@gmail.com>
2022-05-12 23:19:41 +0530

    added put

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -1,7 +1,11 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework import status
+from profiles_api import serializers
+
 
 class HellloAPIView(APIView):
+    serializer_class = serializers.HelloSerializer
 
     def get(self, request, format=None):
         an_apiview = [
@@ -16,3 +20,17 @@ class HellloAPIView(APIView):
             'msg':'Hello',
             'an_apiview': an_apiview
         })
+
+    def post(self, request, format=None):
+        serializer = self.serializer_class(data=request.data)
+        if serializer.is_valid():
+            name = serializer.validated_data.get('name')
+            message = f'Hello {name}'
+            return Response({
+                'msg':message
+            })
+        else:
+            return Response(
+                serializer.errors,
+                status=status.HTTP_400_BAD_REQUEST
+            )
