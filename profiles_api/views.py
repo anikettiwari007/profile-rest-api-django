e23bd3381e860dc4df854d2dc6903f174060cfcd
Aniket <ignou.mt@gmail.com>
2022-05-12 22:20:42 +0530

    added hello world api view

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
 
-# Create your views here.
+class HellloAPIView(APIView):
+
+    def get(self, request, format=None):
+        an_apiview = [
+            'Lorem Ipsum is simply dummy text of the printing and',
+            'typesetting industry. Lorem Ipsum has been the industry',
+            'standard dummy text ever since the 1500s, when an unknown printer took a galley of type and ',
+            'scrambled it to make a type specimen book. ',
+            'It has survived not only five centuries'
+        ]
+
+        return Response({
+            'msg':'Hello',
+            'an_apiview': an_apiview
+        })
